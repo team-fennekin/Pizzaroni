4ba4a8c719c7b4d3971772a2f7c1418701f742e1
Illya Yanchuk <illyayanchuk@Laurens-MacBook-Air.local>
2018-01-10 15:33:43 -0800

    added sizes and crusts

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -8,13 +8,17 @@ class App extends React.Component {
     super(props);
     this.state = { 
       toppings: [{name: 'Pepperoni', price: 2}, {name: 'Mushrooms', price: 1}, {name: 'Bell Peppers', price: 1.25}],
+
       sizes: [{name: 'Small', price: 15}, {name: 'Medium', price: 20}, {name: 'Large', price: 25}],
-      selectedSize: 'option1',
-      crusts: [{name: 'Pan Pizza', price: 3}, {name: 'Deep Dish', price: 5}, {name: 'Neapolitan', price: 4}]
+      selectedSize: null,
+
+      crusts: [{name: 'Pan Pizza', price: 3}, {name: 'Deep Dish', price: 5}, {name: 'Neapolitan', price: 4}],
+      selectedCrust: null
     }
     this.handleSizeChange = this.handleSizeChange.bind(this);
+    this.handleCrustChange = this.handleCrustChange.bind(this);
   }
-
+  
   // componentDidMount() {
   //   $.ajax({
   //     url: '/items', 
@@ -32,6 +36,10 @@ class App extends React.Component {
     this.setState({selectedSize: e.target.value})
   }
 
+  handleCrustChange(e) {
+    this.setState({selectedCrust: e.target.value})
+  }
+
   render () {
     return (
       <div>
@@ -41,17 +49,22 @@ class App extends React.Component {
            <form>
             {this.state.sizes.map((size) => 
               <label>
-                <input type="radio" value="size.name" checked={this.state.selectedSize === size.name} onChange={this.handleSizeChange}/>
+                <input type="radio" value={size.name} checked={this.state.selectedSize === size.name} onChange={this.handleSizeChange}/>
                 {size.name}
               </label>
             )}
            </form>
           </div>
-          <ol id="crust">
+          <div id="crust">
+           <form>
             {this.state.crusts.map((crust) => 
-              <li>{crust.name}</li>
+              <label>
+                <input type="radio" value={crust.name} checked={this.state.selectedCrust === crust.name} onChange={this.handleCrustChange}/>
+                {crust.name}
+              </label>
             )}
-          </ol>
+           </form>
+          </div>
           <div id="toppings">
             {this.state.toppings.map((topping) =>
               <div>{topping.name}: {topping.price}</div>
