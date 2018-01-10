1a67d57eceb43c443e22a2ba837f4a10eb42b1ae
mikestepanov <mikestepanov777@gmail.com>
2018-01-10 12:28:02 -0800

    added basic sql queries

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -7,8 +7,59 @@ var connection = mysql.createConnection({
   database : 'test'
 });
 
-var selectAll = function(callback) {
-  connection.query('SELECT * FROM items', function(err, results, fields) {
+var getAllUsers = function(callback) {
+  connection.query('SELECT * FROM users', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+var getAllOrders = function(callback) {
+  connection.query('SELECT * FROM orders', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+var getAllPizzas = function(callback) {
+  connection.query('SELECT * FROM pizza', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+var getAllCrusts = function(callback) {
+  connection.query('SELECT * FROM cruts', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+
+var getAllSizes = function(callback) {
+  connection.query('SELECT * FROM sizes', function(err, results, fields) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, results);
+    }
+  });
+};
+
+var getAllTopings = function(callback) {
+  connection.query('SELECT * FROM toppings', function(err, results, fields) {
     if(err) {
       callback(err, null);
     } else {
