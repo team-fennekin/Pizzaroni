43e08807977e8ec2dc7a59f10a7b0e66c77883ef
mikestepanov <mikestepanov777@gmail.com>
2018-01-12 13:59:22 -0800

    commit

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Pizza from './components/Pizza.jsx';
-import Log from './components/Log.jsx';
+// import Log from './components/Log.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,7 +18,6 @@ class App extends React.Component {
     return (
       <div>
         <Pizza />
-        <Log />
       </div>
     );
   }
