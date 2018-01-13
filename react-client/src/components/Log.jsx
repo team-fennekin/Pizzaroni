167707d851281e9ee567d3ec2eb6e4fbf05c521f
mikestepanov <mikestepanov777@gmail.com>
2018-01-12 17:01:55 -0800

    finished skeleton for Log

--- react-client/src/components/Log.jsx
+++ react-client/src/components/Log.jsx
@@ -6,24 +6,30 @@ class Log extends React.Component  {
 
     this.state = {
     };
+
+    this.sendRequest = this.sendRequest.bind(this);
+  }
+
+  sendRequest() {
+    
   }
 
   render() {
     return (
       <div>
-        <img src='fat_unicorn.png'/>
-        <img src='big_white_unicorn.jpg'/>
-        <img src='sad_unicorn.png'/>
-        <img src='white_unicorn.jpg'/>
         <h1>Try to not get hit by rainbows</h1>
         <h2>Username</h2>
         <input />
         <h2>Password</h2>
         <input />
-        <button></button>
+        <button onClick={this.sendRequest}>Submit</button>
       </div>
     );
   }
 }
 
+// <img src='fat_unicorn.png'/>
+// <img src='big_white_unicorn.jpg'/>
+// <img src='sad_unicorn.png'/>
+// <img src='white_unicorn.jpg'/>
 export default Log;
