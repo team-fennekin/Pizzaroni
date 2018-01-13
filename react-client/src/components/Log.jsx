1158451d166dbac0beedffb43443921f15826b5f
mikestepanov <mikestepanov777@gmail.com>
2018-01-12 16:58:49 -0800

    protected from rainbow tables

--- react-client/src/components/Log.jsx
+++ react-client/src/components/Log.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Log extends React.Component {
-  consructor(props) {
+class Log extends React.Component  {
+  constructor(props) {
     super(props);
 
     this.state = {
-
     };
   }
 
   render() {
     return (
       <div>
-        <h1>Try to not get hit by rainbows ;)</h1>
-
+        <img src='fat_unicorn.png'/>
+        <img src='big_white_unicorn.jpg'/>
+        <img src='sad_unicorn.png'/>
+        <img src='white_unicorn.jpg'/>
+        <h1>Try to not get hit by rainbows</h1>
         <h2>Username</h2>
         <input />
         <h2>Password</h2>
@@ -25,5 +26,4 @@ class Log extends React.Component {
   }
 }
 
-
-ReactDOM.render(<Log />, document.getElementById('log'));
+export default Log;
