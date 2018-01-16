3b1f4a93f5a903d844221bd8e0aa793afd4c05ca
Suhail Ansari <suhailansari@gmail.com>
2018-01-15 19:45:15 -0800

    made progressbar stateless

--- react-client/src/components/ProgressBar.jsx
+++ react-client/src/components/ProgressBar.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 
-class ProgressBar extends React.Component {
-  constructor(props) {
-    super(props);
+const ProgressBar = (props) => (
+  <div id="progress-bar">
+    <h2>Progress Bar</h2>
+    <div id={(props.currentStep > 0) ? 'completed' : 'not-completed'}>Size</div>
+    <div id={(props.currentStep > 1) ? 'completed' : 'not-completed'}>Crust</div>
+    <div id={(props.currentStep > 2) ? 'completed' : 'not-completed'}>Toppings</div>
 
-  }
+    <div id="next-button">
+      <button onClick={props.nextOption}>Next</button>
+    </div>
 
-  render() {
-    return (
-      <div id="progress-bar">
-        <h2>Progress Bar</h2>
-        <div id={(this.props.currentStep > 0) ? 'completed' : 'not-completed'}>Size</div>
-        <div id={(this.props.currentStep > 1) ? 'completed' : 'not-completed'}>Crust</div>
-        <div id={(this.props.currentStep > 2) ? 'completed' : 'not-completed'}>Toppings</div>
-
-        <div id="next-button">
-          <button onClick={this.props.nextOption}>Next</button>
-        </div>
-
-        <div id="back-button">
-          <button onClick={this.props.backOption}>Back</button>
-        </div>
-      </div>
-    )
-  }
-}
+    <div id="back-button">
+      <button onClick={props.backOption}>Back</button>
+    </div>
+  </div>
+)
 
 export default ProgressBar;
