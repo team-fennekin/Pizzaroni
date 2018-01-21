892e1c7d40467730adcb88c3e8dde79b0df40b0f
illyay2017 <illya.yanchuk@gmail.com>
2018-01-20 21:00:36 -0800

    lowerCase p

--- react-client/src/components/PizzaPicture.jsx
+++ react-client/src/components/PizzaPicture.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PizzaPicture = (props) => (
   <div id="pizza-picture">
-    <img src="../pictures/pizza.png" />
+    <img src="../pictures/Pizza.png" />
   </div>
 )
 
